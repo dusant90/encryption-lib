655479d09a203ca930114f2977702b71032ee979
Dusan Tomic <dusan.tomic@wolterskluwer.com>
2021-02-27 00:11:19 +0100

    Resolved issue with running the app from cli

--- encrypt/src/main/java/com/base/encrypt/EncryptApplication.java
+++ encrypt/src/main/java/com/base/encrypt/EncryptApplication.java
@@ -1,11 +1,14 @@
 package com.base.encrypt;
 
+import com.base.encrypt.controller.EncryptionController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"com/base/encrypt/"})
+@ComponentScan(basePackageClasses = EncryptionController.class)
+
 public class EncryptApplication {
 
 	public static void main(String[] args) {
