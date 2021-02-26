d78da46aeb4d20b760a1bd9a164d1f4020c61855
Dusan Tomic <dusan.tomic@wolterskluwer.com>
2021-02-26 16:39:10 +0100

    Added postman collection for FIle Handler API

--- filehandler-app/src/test/java/com/demo/app/filehandler/api/service/FileServiceTest.java
+++ filehandler-app/src/test/java/com/demo/app/filehandler/api/service/FileServiceTest.java
@@ -1,9 +1,4 @@
 package com.demo.app.filehandler.api.service;
-import encrypt.EncryptionFailedException;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.io.IOException;
 
 public class FileServiceTest {
 
